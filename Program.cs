d0fa1980c7994763a817e818a2d75b75e3249c59
DevDiex <guners302@gmail.com>
2026-08-23 22:44:26 +0300

    Калькулятор с if-else примитив интерактив

--- Program.cs
+++ Program.cs
@@ -4,27 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        int o = 0;
-        int a = 5;
-        int b = 10;
-        int c = 265;
-        int h = 250;
-        int g = (a + b + c + c);
-        int sum = (h + h); // Баланс банка
-        int sum2 = (sum - c); // (500 - 265 = 235) новый баланс банка после перевода
-        int sum3 = (o + c); // сумма на нашей карте (0 + 265)
-        int sum4 = sum2; // баланс банка
-        Console.WriteLine($"Баланс банка равен: {sum}.");
-        Console.WriteLine($"Баланс банка изменен - сумма успешно переведена: {sum2}.");
-        Console.WriteLine($"Сумма на карте равна: {sum3}.");
 
-        if (sum3 >= h)
+        while (true) // бесконечный цикл крутящий код внутри {} чтобы из него выйти нужен оператор break и чтобы прыгнуть в него игнорируя код снизу нужен continue;
         {
-            int sum5 = (sum3 - h);
-            Console.WriteLine($"Покупка успешна, Сумма списана! {sum5}.");
-        }
-        else
-            Console.WriteLine($"Недостаток средств: СУММА НЕ МОЖЕТ БЫТЬ СПИСАНА (тест)");
-    }
-}
+            Console.Write("Введите первое число: "); // Write не переносит строку как Enter как WriteLine
+            string? input1 = Console.ReadLine(); // Считываем что ввели на клавиатуре и сохраняем как строку из текста
 
+            if (int.TryParse(input1, out int num1) == false) // Сразу переводим в число (если ввели букву - ошибка) и out int num1 удобно на лету создать переменную.
+            {
+                Console.WriteLine("Ошибка");
+                Console.ReadKey(); // Ждем нажатия кнопки
+
+                continue; // Возвращаем принудительно программу в самое начало цикла while
+
+            }
+
+            Console.WriteLine("Что вводим? (+, -, *, /, exit,); ");
+            string? op = Console.ReadLine(); // Считываем введеный математический знак (+, -, *, /, exit) и сохраняем в строку op
+
+            if (op == "exit")
+            {
+                break;
+            }
+
+            // Запросим второе число
+            Console.Write("Введите второе число: ");
+            string? input2 = Console.ReadLine();
+
+            if (int.TryParse(input2, out int num2) == false)
+            {
+                Console.WriteLine("Ошибка");
+                Console.ReadKey();
+
+                continue;
+            }
+            int result = 0;
+
+            if (op == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (op == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (op == "*")
+            {
+                result = num1 * num2;
+            }
+
+            else if (op == "/")
+            {
+                result = num1 / num2;
+            }
+            // Условия (conditions - if/else if) проверяемые компом сверху вниз (from top to bottom)
+            // если сработает один if остальные компьютер скипнет.
+            // == проверка сравнение (точно ли) а не присвоение как =
+            Console.WriteLine($"Результат операций: {result}");
+            Console.WriteLine($"Нажми любую кнопку для следующего круга...");
+            Console.ReadKey();
+        }
+    }
+    // Main главная точка входа в программу с которой все начинается (в бэкэнде так не пишут)
+}
