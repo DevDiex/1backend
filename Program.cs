6de824d884ae6b5c455ab6f9f75372b8047df10b
DevDiex <guners302@gmail.com>
2026-09-18 16:24:35 +0300

    Попытка совмещения изолированных методов и практики на примере с бэкэнд серверной логикой ошибок с сортировкой от меньших ошибок к большим

--- Program.cs
+++ Program.cs
@@ -19,3 +19,72 @@
 
 
 
+// List<string> serverLogs = new List<string>();
+
+//     serverLogs.Add("INFO: Server Started"); // 0
+//     serverLogs.Add("Warning: Low Memory"); // 1
+//     serverLogs.Add("ERROR: Database connection failed"); // 2
+//     serverLogs.Add("INFO: User logged in"); // 3
+//     serverLogs.Add("ERROR: Access denied"); // 4
+//     // 5 обьектов
+
+//     while (true)
+//     {
+
+//         Console.WriteLine("Выберите опцию: 1 - Показать Все логи");
+//         Console.WriteLine("Выберите опцию: 2 - Показать Только Ошибки");
+//         Console.WriteLine("Выберите опцию: 3 - Удалить все предупреждения");
+//         Console.WriteLine("Выберите опцию: 4 - Выйти (STOP)");
+
+//         Output(serverLogs);
+
+//     }
+// }
+
+// static void Output(List<string> strings)
+// {
+//     string? Optionic1 = Console.ReadLine();
+
+//     switch (Optionic1)
+//     {
+//         case "1":
+//             for (int i = 0; i < strings.Count; i++)
+//             {
+//                 Console.WriteLine($"Ваши логи: {strings[i]}");
+//                 Console.WriteLine("");
+//             }
+//             break;
+//         case "2":
+//             for (int i = 0; i < strings.Count; i++)
+//             {
+//                 if (strings[i].StartsWith("ERROR:"))
+//                 {
+//                     Console.WriteLine($"Ваши ошибки: {strings[i]}");
+//                 }
+//             }
+//             break;
+//         case "3":
+//             for (int i = strings.Count - 1; i >= 0; i--)
+//             {
+//                 if (strings[i].StartsWith("Warning:"))
+//                 {
+//                     strings.RemoveAt(i);
+//                 }
+//             }
+
+//             Console.WriteLine($"Предупреждения удалены: Оставшиеся логи сервера");
+//             for (int i = 0; i < strings.Count; i++)
+//             {
+//                 Console.WriteLine($"Ваши логи {strings[i]}");
+//             }
+
+//             break;
+//         case "4":
+//             string? Checker = Console.ReadLine();
+//             if (Checker == "STOP")
+//             {
+//                 Environment.Exit(0);
+//             }
+//             break;
+
+//     }
--- ScoresMiniGame.cs
+++ ScoresMiniGame.cs
@@ -5,86 +5,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<string> NamesMain = new List<string>();
+        Console.WriteLine(" \n Бэкэнд серверная логика");
+        int[] serverLogHours = { 12, 45, 5, 90, 23, 150, 4 };
+        int sum = 0;
 
-        NamesMain.Add("Admin_Darmek");
-        NamesMain.Add("Admin_Remlik");
-        NamesMain.Add("Dima");
-        NamesMain.Add("Ivan");
-        NamesMain.Add("Admin_SanyaSinShluxi");
 
-        while (true)
+        Console.WriteLine($"\nОшибки в массиве");
+        PrintArray(serverLogHours);
+
+        foreach (int num in serverLogHours)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"Количество ошибок в этом дне: {sum}");
+
+        BackEndSort(serverLogHours);
+        Console.WriteLine();
+
+        SortedList(serverLogHours);
+        SortedConsole(serverLogHours);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
         {
 
+            Console.WriteLine($"Ошибок {arr[i]} в часу {i}");
+        }
+        Console.WriteLine();
+    }
 
-
-            Console.WriteLine("\n Введите имя пользователя или выйдите из программы командой STOP: ");
-            string? Names = Console.ReadLine() ?? "";
-            if (Names == "STOP")
+    static void BackEndSort(int[] arr)
+    {
+        Console.WriteLine($" \n Проверка критических ошибок (i > 50) =:");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] >= 50)
             {
-                break;
+                Console.WriteLine($"Критических ошибок: Количество {arr[i]} больше 50 на часу [{i + 1}]");
             }
-
-            if (int.TryParse(Names, out int Namenum) == true)
-
+        }
+    }
+    static void SortedList(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length - 1; j++)
             {
-                Console.WriteLine("Error");
-                Console.ReadKey();
-                continue;
-
-            }
-
-            if (Names == "")
-            {
-                Console.Write("Ошибка: Для повторной попытки нажмите любую клавишу  ");
-                Console.ReadKey();
-                continue;
-            }
-            bool isUserAdmin = Names.StartsWith("Admin_");
-
-            if (isUserAdmin)
-            {
-                Console.WriteLine($"Вы админ - вот вам все известные админы: ");
-            }
-            else
-            {
-                Console.WriteLine("Вы обычный гость: Вот список");
-            }
-
-            foreach (string Nemesis in NamesMain)
-            {
-                bool ListContainAdmin = Nemesis.StartsWith("Admin_");
-
-                if (isUserAdmin && ListContainAdmin)
+                if (arr[j] > arr[j + 1])
                 {
-                    Console.WriteLine($"- {Nemesis}");
-                }
-                else if (!isUserAdmin && !ListContainAdmin)
-                {
-                    Console.WriteLine($"- {Nemesis}");
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-
-            Console.WriteLine(" - Введите команду для использования функций - ");
-            string? op2 = Console.ReadLine();
-
-            if (op2 == "-%Ad.Cls%-")
-            {
-                for (int i = NamesMain.Count - 1; i >= 0; i--)
-                {
-                    if (NamesMain[i].StartsWith("Admin_"))
-                    {
-                        NamesMain.RemoveAt(i);
-                    }
-                }
-
-
-                Console.WriteLine($"- List was cleaned from Admins - ");
-                foreach (string CleanedNames in NamesMain)
-                {
-                    Console.WriteLine($"SortedNames: {CleanedNames}");
-                }
-            }
+        }
+    }
+    static void SortedConsole(int[] arr)
+    {
+        foreach (int errors in arr)
+        {
+            Console.WriteLine($"{errors}");
         }
     }
 }
