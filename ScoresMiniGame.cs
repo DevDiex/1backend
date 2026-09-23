0d894452c16c79d6c203b5a70b96d22f62714dff
DevDiex <guners302@gmail.com>
2026-09-23 22:49:29 +0300

    OOP testing newbie classes for RPG inventory system, need way more practice, hard work with console.writeline and add.(new Item {})

--- ScoresMiniGame.cs
+++ ScoresMiniGame.cs
@@ -1,100 +1,110 @@
 using System.Globalization;
 namespace MyConsoleApp;
 
-// Класс (class) - просто чертеж, шаблон. Ничего не делает кроме обьяснения компьютеру - если хотим создать сотрудника у него обязательно должны быть такие свойства
-// public - публичный, Позволяет читать компьютеру и менять данные из любой точки программы (если не писать public class - ничего страшного), внутри обязательно писать publc иначе не сможет из отдельного метода и класса вызываться.
-public class EmployeeLog
+public class Item
 {
-    public string? Name { get; set; } = string.Empty;
-    public string? Status { get; set; } = string.Empty;
-    public int Time { get; set; } // Числа (int) по умолчанию равны 0, тут предупреждения нет
-    public string? City { get; set; } = string.Empty;
+    public string? Name { get; set; } = string.Empty; // Название Предмета
+    public string? Rarity { get; set; } = string.Empty; // Редкость
+    public int DamageBonus { get; set; } // Бонус к атаке
 }
+
 class Program
 {
     static void Main(string[] args)
     {
-        // Создание списка обьектов вместо списка безликих строк.
-        List<EmployeeLog> CalledNames = new List<EmployeeLog>();
+        List<Item> Inventory = new List<Item>();
 
-        // new - команда: Выделить память и создать новый живой обьект по чертежу EmployeeLog
+        Inventory.Add(new Item { Name = "Ржавый кинжал", Rarity = "Basic", DamageBonus = 2 });
+        Inventory.Add(new Item { Name = "Посох Огня", Rarity = "Rare", DamageBonus = 15 });
+        Inventory.Add(new Item { Name = "Испепелитель", Rarity = "Epic", DamageBonus = 45 });
 
-        CalledNames.Add(new EmployeeLog { Name = "Eugene", Status = "Admin", City = "Мосвка", Time = 10 });
-        CalledNames.Add(new EmployeeLog { Name = "Krabst", Status = "Guest", City = "Екатеринбург", Time = 11 });
-        CalledNames.Add(new EmployeeLog { Name = "Evgene", Status = "Admin", City = "Москва", Time = 15 });
-        CalledNames.Add(new EmployeeLog { Name = "Mariana", Status = "Guest", City = "seventeen", Time = 5 });
-        CalledNames.Add(new EmployeeLog { Name = "DeanW", Status = "Admin", City = "Сан-Франциско", Time = 15 });
+
 
         while (true)
         {
-            Console.WriteLine();
-            Console.WriteLine("== Система Контроля Доступа ==");
-            Console.WriteLine("1 - Показать все логи");
-            Console.WriteLine("2 - Показать только Админов из Екатеринбурга");
-            Console.WriteLine("3 - Удалить гостей, пришедших после 10 ");
-            Console.WriteLine("4 - Выйти (STOP)");
-            Console.WriteLine();
-            Console.WriteLine("Выберите Опцию");
+            Console.WriteLine("-- ИНВЕНТАРЬ ГЕРОЯ--");
+            Console.WriteLine("1 - Показать весь инвентарь");
+            Console.WriteLine("2 - Показать только Эпические предметы");
+            Console.WriteLine("3 - Введите предмет в инвентарь");
+            Console.WriteLine("4 - Выйти из игры");
+            Console.Write("Выберите опцию: ");
 
-            ProcessMenu(CalledNames);
-        }
+            string? choice = Console.ReadLine();
 
-    }
-    static void ProcessMenu(List<EmployeeLog> logs)
-    {
-        string? option = Console.ReadLine();
-
-        switch (option)
-        {
-            case "1":
-                for (int i = 0; i < logs.Count; i++)
-                {
-                    Console.WriteLine($"Сотрудник : {logs[i].Name} | Статус: {logs[i].Status} | Время: {logs[i].Time}, Город: {logs[i].City}");
-                }
-                Console.Write("Нажмите клавишу для перезапуска программы: ");
-                Console.ReadKey();
-
-                break;
-
-            case "2":
-                for (int i = 0; i < logs.Count; i++)
-                {
-                    if (logs[i].Status == "Admin" && logs[i].City == "Москва")
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("-- Ваши предметы --");
+                    for (int i = 0; i < Inventory.Count; i++)
                     {
-                        Console.WriteLine($"Админ: {logs[i].Name} из города {logs[i].City}");
+                        Console.WriteLine($"[{Inventory[i].Rarity}], [{Inventory[i].Name}], [{Inventory[i].DamageBonus}]");
                     }
-                }
-                break;
-            case "3":
-                for (int i = logs.Count - 1; i >= 0; i--)
-                {
-                    // Убираем trim() и Contains (относящееся к List<>) ведь теперь мы работаем с фиксированными функциями/обьектами (int, string. etc) записанные в нашем отдельном классе
-
-
-                    if (logs[i].Status == "Guest" && logs[i].Time > 10)
+                    break;
+                case "2":
+                    Console.WriteLine("-- Ваши эпические предметы --");
+                    for (int i = 0; i < Inventory.Count; i++)
                     {
-                        Console.WriteLine($"Удален гость: {logs[i].Name}");
-                        logs.RemoveAt(i); // RemoveAt вместо Remove из-за наличия удаления за счет индексов
+                        if (Inventory[i].Rarity == "Epic")
+                        {
+                            Console.WriteLine($"🔥{Inventory[i].Name}🔥. Его урон [{Inventory[i].DamageBonus}] и его редкость [{Inventory[i].Rarity}]");
+                        }
+                    }
+                    break;
+                case "3":
+                    if (Inventory.Count > 5)
+                    {
+                        Console.WriteLine("Ошибка! инвентарь заполнен");
+                        Console.ReadKey();
+                        break;
                     }
 
-                }
-                break;
+                    Console.WriteLine("== Добавление предмета");
 
-            case "4":
-                Console.WriteLine("Введите STOP");
-                string? command = Console.ReadLine();
+                    Console.Write("Введите название предмета: ");
+                    string? item1 = Console.ReadLine();
 
-                if (command == "STOP")
-                {
-                    Environment.Exit(0);
-                }
-                break;
+                    if (int.TryParse(item1, out int num1) == true || string.IsNullOrWhiteSpace(item1))
+                    {
+                        Console.WriteLine("Ошибка: Некоректное название предмета ");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    Console.Write("Введите редкость из выпадающего списка: (Basic, Rare, Epic): ");
+                    string? Rarity2 = Console.ReadLine();
+
+                    if (Rarity2 != "Basic" && Rarity2 != "Rare" && Rarity2 != "Epic")
+                    {
+                        Console.WriteLine("Ошибка: Неверная редкость.");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    Console.Write("Введите Бонус к атаке (число): ");
+                    if (!int.TryParse(Console.ReadLine(), out int damage) || damage < 0)
+                    {
+                        Console.WriteLine("Ошибка: Бонус должен быть положительным числом");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    Item newItem = new Item
+                    {
+                        Name = item1,
+                        Rarity = Rarity2,
+                        DamageBonus = damage
+                    };
+                    Inventory.Add(newItem);
+                    Console.WriteLine($"Предмет [{newItem.Name}], ({newItem.Rarity}) с уроном +{newItem.DamageBonus} добавлен в инвентарь.");
+                    Console.WriteLine($"Мест занято: {Inventory.Count}/5");
+                    Console.ReadKey();
+                    break;
+
+
+                case "4":
+                    Console.WriteLine("Выход из игры...");
+                    return; // Завершаем метод Main и закрываем программу
+            }
         }
     }
 }
-
-// Свойство точка (.) - Главный инструмент ООП
-// Когда элемент лежит в списке, компьютер знает что logs[i] - не просто текст а целый обьект
-// поставив точку после уточнения обьекта мы можем получить доступ к его внутренностям
-// logs[i].Name - Вытащит имя
-// logs[i].Time - Вытащит имя, по итогу мы избавляемся от склейки ручной строк split("|") и Trim() и не рискуем ошибиться с индексами + Данные типизированы и легко считаются человеком + защищены
